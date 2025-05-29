9934bf133ed28c8492f663ca24fc0ec26d61c04e
zoli0682 <zoli0682@sydney.edu.au>
2025-05-29 18:58:18 +1000

    Add SemiCircle class,Modify Sketch.js
    Add a class folder and SemiCircle class.
    Add 3 array in the sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,12 @@
+// The Array of Point
+let pointArray = [];
+
+// The Array of Line
+let lineArray = [];
+
+// The Array of SemiCircle
+let semiCircleArray = [];
+
 function setup() {
   createCanvas(400, 400);
 }
