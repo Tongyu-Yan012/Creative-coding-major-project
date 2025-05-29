22b991a4e460ac796f1463113ef7ae70f0aefb6a
zoli0682 <zoli0682@sydney.edu.au>
2025-05-29 22:22:14 +1000

    Modify in SemiCircle.js
    Add Comment

--- class/SemiCircle.js
+++ class/SemiCircle.js
@@ -1,4 +1,5 @@
 class SemiCircle {
+  //https://editor.p5js.org/aartsyfaartsy/sketches/Go8edGRsZ
   /**
    * The parameter need you input the point1 and the point2
    * Also you can input the color1, color2 and borderWeight
@@ -16,7 +17,9 @@ class SemiCircle {
     this.color2 = color2;
     this.cx = (this.p1.x + this.p2.x) / 2;
     this.cy = (this.p1.y + this.p2.y) / 2;
+    //https://p5js.org/reference/p5/dist/
     this.diameter = dist(this.p1.x, this.p1.y, this.p2.x, this.p2.y);
+    //https://p5js.org/reference/p5/atan2/
     this.angle = atan2(this.p2.y - this.p1.y, this.p2.x - this.p1.x);
     this.borderWeight = borderWeight;
   }
@@ -29,6 +32,7 @@ class SemiCircle {
     noFill();
     stroke(0);
     strokeWeight(this.borderWeight);
+    // https://p5js.org/reference/p5/arc/
     arc(0, 0, this.diameter, this.diameter, -PI / 2, PI / 2, PIE);
 
     // Half Circle
