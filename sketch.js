ccfa10f8f72b788487d0bd0013269c0e20d34348
kche0637 <kche0637@uni.sydney.edu.au>
2025-05-30 00:49:38 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,16 @@
 // The Array of Point
+const rawCoords = [
+  { x: 300, y: 595 },  { x: 246, y: 591 },  { x: 194, y: 589 },  { x: 351, y: 596 },
+  { x: 406, y: 595 },  { x: 306, y: 557 },  { x: 304, y: 526 },  { x: 304, y: 490 },
+  { x: 306, y: 411 },  { x: 307, y: 329 },  { x: 309, y: 314 },  { x: 313, y: 289 },
+  { x: 314, y: 246 },  { x: 270, y: 246 },  { x: 245, y: 244 },  { x: 268, y: 212 },
+  { x: 360, y: 245 },  { x: 360, y: 216 },  { x: 348, y: 329 },  { x: 384, y: 333 },
+  { x: 421, y: 333 },  { x: 427, y: 272 },  { x: 429, y: 239 },  { x: 434, y: 183 },
+  { x: 434, y: 154 },  { x: 461, y: 159 },  { x: 504, y: 172 },  { x: 534, y: 183 },
+  { x: 539, y: 142 },  { x: 180, y: 327 },  { x: 239, y: 328 },  { x: 278, y: 329 },
+  { x: 131, y: 136 },  { x: 135, y: 174 },  { x: 188, y: 172 },  { x: 182, y: 212 },
+  { x: 181, y: 258 },  { x: 127, y: 84 }
+];
 let pointArray = [];
 
 // The Array of Line
@@ -31,11 +43,17 @@ function setup() {
   b = random(255);      
   
   background(32);
+  for (let pt of rawCoords) {
+    pointArray.push(new Point(pt.x, pt.y, 8));
+  }
 }
 
 function draw() {
   for (let i = 0; i < 1; i++) {
     drawRandomLine();
+  }
+  for (let p of pointArray) {
+    p.display();  
   }
 }
 
