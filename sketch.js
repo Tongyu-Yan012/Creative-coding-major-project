8a4905fdd07b796e92eca7a9cf3338f778dd2b79
kche0637 <kche0637@uni.sydney.edu.au>
2025-05-30 22:49:21 +1000

    Remove and modify point related code
    Removed all code related to point display, retaining only the necessary structures for segment and branch generation; preserved and ensured the integrity of the overall tree or structure; focused functionality on the generation of branches and lines.

--- sketch.js
+++ sketch.js
@@ -1,23 +1,37 @@
+let pointArray = [];
+
 // ground Array
-let groundPointArray = [new Point(300, 595), new Point(246,  591), new Point ( 194,  589), new Point  (351,  596), new Point  (406,  595) ];
+let groundPointArray = [];
 
 // The first branch Arrary
-let firstBranchArray = [new Point(300, 595), new Point ( 306,  557 ),new Point ( 304,  526),new Point  (304,  490 ),new Point  (306,  411),new Point  (307,  329 )];
+let firstBranchArray = [];
 
 // The first left Branch
-let leftBranch = [new Point(127, 84), new Point(131, 136),  new Point(135, 174), new Point(188, 172), new Point(182, 212), new Point(181, 258),   new Point(180, 327),  new Point(239, 328),  new Point(278, 329), new Point  (307,  329 )];
+let leftBranch = [];
+
+// The left up branch
+let leftUpBranch = [];
 
 // The first right Branch
-let rightBranch = [new Point  (307,  329 ), new Point (348,  329),  new Point  (384,  333),  new Point  (421,  333),  new Point  (427,  272),  new Point  (429,  239),  new Point  (434,  183),  new Point  (434,  154),  new Point  (461,  159),  new Point  (504,  172),  new Point  (534,  183),  new Point  (539,  142 )];
+let rightBranch = [];
+
+// The right up Branch
+let rightUpBranch = [];
 
 // The second up branch Array
-let secondUpBranch = [new Point  (307,  329 ), new Point  (309,  314),  new Point  (313,  289),  new Point  (314,  246)];
+let secondUpBranch = [];
 
 // The second left branch
-let secondLeftBranch = [new Point  (314,  246), new Point  (270,  246),new Point  (245,  244),  new Point  (268,  212)];
+let secondLeftBranch = [];
+
+// The second left Up branch
+let secondLeftUpBranch = [];
 
 // The second Right Branch
-let secondRightBranch = [new Point  (314,  246), new Point  (360,  245),  new Point  (360,  216)];
+let secondRightBranch = [];
+
+// The second Right Up Branch
+let secondRightUpBranch = [];
 
 // The Array of Line
 let lineArray = [];
@@ -42,7 +56,9 @@ function setup() {
   xPos = width / 2;
   yPos = height / 2;
   background(25);
+  let firstStartPointOfTheGround = new Point(200, 595);
 
+  setThePoint(firstStartPointOfTheGround);
 
   for(let i=0; i < groundPointArray.length - 1; i++){
     let p1 = groundPointArray[i];
@@ -101,27 +117,7 @@ function draw() {
     lineArray[i].display();
   }
 
-  for (let i = 0;i < groundPointArray.length; i++) {
-    groundPointArray[i].display()
-  }
-  for (let i = 0;i < firstBranchArray.length; i++) {
-    firstBranchArray[i].display()
-  }
-  for (let i = 0;i < secondUpBranch.length; i++) {
-    secondUpBranch[i].display()
-  }
-  for (let i = 0;i < leftBranch.length; i++) {
-    leftBranch[i].display()
-  }
-  for (let i = 0;i < rightBranch.length; i++) {
-    rightBranch[i].display()
-  }
-  for (let i = 0;i < secondLeftBranch.length; i++) {
-    secondLeftBranch[i].display()
-  }
-  for (let i = 0;i < secondRightBranch.length; i++) {
-    secondRightBranch[i].display()
-  }
+  
 }
 
 function drawRandomLine() {
@@ -150,10 +146,140 @@ function drawRandomLine() {
   yPos = nextY;
   pop();
 }
+function setThePoint(firstStartPointOfTheGround) {
+  groundPointArray[0] = firstStartPointOfTheGround;
 
-function getRandomValueUsePerlin(MagnificationPara = 10) {
+  //Ground Point
+  for (let i = 0; i < 4; i++) {
+    let newPointOfTheGround = new Point(
+      groundPointArray[i].x + getRandomValueUsePerlin(50, 60, 1),
+      groundPointArray[i].y + getRandomValueUsePerlin(1, 2)
+    );
+    groundPointArray[i + 1] = newPointOfTheGround;
+  }
+
+  let theMiddlePointOfTheGround =
+    groundPointArray[floor(groundPointArray.length / 2)];
+  firstBranchArray[0] = theMiddlePointOfTheGround;
+
+  //First Branch Point
+  for (let i = 0; i < 10; i++) {
+    let newPointOfTheFirstBranch = new Point(
+      firstBranchArray[i].x + getRandomValueUsePerlin(1, 5),
+      firstBranchArray[i].y + getRandomValueUsePerlin(20, 30, -1)
+    );
+    firstBranchArray[i + 1] = newPointOfTheFirstBranch;
+  }
+
+  let theFirstCrossPoint = firstBranchArray[firstBranchArray.length - 1];
+  leftBranch[0] = theFirstCrossPoint;
+  rightBranch[0] = theFirstCrossPoint;
+  secondUpBranch[0] = theFirstCrossPoint;
+
+  // Left Branch Point
+  for (let i = 0; i < 8; i++) {
+    let newPointOfTheLeftBranch = new Point(
+      leftBranch[i].x + getRandomValueUsePerlin(10, 20, -1),
+      leftBranch[i].y + getRandomValueUsePerlin(1, 5, -1)
+    );
+    leftBranch[i + 1] = newPointOfTheLeftBranch;
+  }
+
+  leftUpBranch[0] = leftBranch[leftBranch.length - 1];
+
+  // Left Up Branch Point
+  for (let i = 0; i < 8; i++) {
+    let newPointOfTheLeftUpBranch = new Point(
+      leftUpBranch[i].x + getRandomValueUsePerlin(1, 5),
+      leftUpBranch[i].y + getRandomValueUsePerlin(5, 10, -1)
+    );
+    leftUpBranch[i + 1] = newPointOfTheLeftUpBranch;
+  }
+
+  // Right Branch Point
+  for (let i = 0; i < 8; i++) {
+    let newPointOfTheRightBranch = new Point(
+      rightBranch[i].x + getRandomValueUsePerlin(10, 15, 1),
+      rightBranch[i].y + getRandomValueUsePerlin(1, 5, -1)
+    );
+    rightBranch[i + 1] = newPointOfTheRightBranch;
+  }
+
+  rightUpBranch[0] = rightBranch[leftBranch.length - 1];
+
+  // Right Up Branch Point
+  for (let i = 0; i < 8; i++) {
+    let newPointOfTheRightUpBranch = new Point(
+      rightUpBranch[i].x + getRandomValueUsePerlin(1, 5),
+      rightUpBranch[i].y + getRandomValueUsePerlin(5, 10, -1)
+    );
+    rightUpBranch[i + 1] = newPointOfTheRightUpBranch;
+  }
+
+  //Second Branch Point
+  for (let i = 0; i < 4; i++) {
+    let newPointOfTheSecondBranch = new Point(
+      secondUpBranch[i].x + getRandomValueUsePerlin(1, 5),
+      secondUpBranch[i].y + getRandomValueUsePerlin(10, 20, -1)
+    );
+    secondUpBranch[i + 1] = newPointOfTheSecondBranch;
+  }
+
+  let theSecondCrossPoint = secondUpBranch[secondUpBranch.length - 1];
+  secondLeftBranch[0] = theSecondCrossPoint;
+  secondRightBranch[0] = theSecondCrossPoint;
+
+  //SecondLeftBranch
+  for (let i = 0; i < 4; i++) {
+    let basePoint = secondLeftBranch[i];
+    let newPointOfTheSecondLeft = new Point(
+      basePoint.x + getRandomValueUsePerlin(10, 20, -1),
+      basePoint.y + getRandomValueUsePerlin(1, 5, -1)
+    );
+    secondLeftBranch[i + 1] = newPointOfTheSecondLeft;
+  }
+
+  secondLeftUpBranch[0] = secondLeftBranch[secondLeftBranch.length - 1];
+
+  //Second Left Up Branch Point
+  for (let i = 0; i < 3; i++) {
+    let newPointOfTheSecondLeftUpBranch = new Point(
+      secondLeftUpBranch[i].x + getRandomValueUsePerlin(1, 5),
+      secondLeftUpBranch[i].y + getRandomValueUsePerlin(5, 10, -1)
+    );
+    secondLeftUpBranch[i + 1] = newPointOfTheSecondLeftUpBranch;
+  }
+
+  //secondRightBranch
+  for (let i = 0; i < 4; i++) {
+    let basePoint = secondRightBranch[i];
+    let newPointOfTheSecondRight = new Point(
+      basePoint.x + getRandomValueUsePerlin(10, 20, 1),
+      basePoint.y + getRandomValueUsePerlin(1, 5, -1)
+    );
+    secondRightBranch[i + 1] = newPointOfTheSecondRight;
+  }
+
+  secondRightUpBranch[0] = secondRightBranch[secondRightBranch.length - 1];
+
+  //Second Right Up Branch Point
+  for (let i = 0; i < 3; i++) {
+    let newPointOfTheSecondRightUpBranch = new Point(
+      secondRightUpBranch[i].x + getRandomValueUsePerlin(1, 5),
+      secondRightUpBranch[i].y + getRandomValueUsePerlin(5, 10, -1)
+    );
+    secondRightUpBranch[i + 1] = newPointOfTheSecondRightUpBranch;
+  }
+}
+
+function getRandomValueUsePerlin(
+  min = 0,
+  max = 20,
+  NegativeAndPositiveNum = random()
+) {
   let n = noise(0.01);
-  let offset = n * MagnificationPara;
+  let value = NegativeAndPositiveNum < 0.5 ? -1 : 1;
+  let offset = ceil(map(n, 0, 1, min, max) * value);
 
   return offset;
 }
