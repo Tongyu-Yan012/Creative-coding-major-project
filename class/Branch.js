2f71084d61ba301afeafa1b7a3f1dbd2c3f3e5ca
Tongyu-Yan012 <tyan0046@uni.sydney.edu.au>
2025-05-30 15:33:53 +1000

    linearray

--- class/Branch.js
+++ class/Branch.js
@@ -9,7 +9,7 @@ class Branch{
   display() {
   stroke(this.stroke)
   strokeWeight(this.strokeWeight)
-  line(p1.x1, p1.y1, p2.x2, p2.y2);
+  line(this.p1.x, this.p1.y, this.p2.x, this.p2.y);
   }
 }
 
--- sketch.js
+++ sketch.js
@@ -1,44 +1,8 @@
-// The Array of Point
-const rawCoords = [
-  { x: 300, y: 595 },
-  { x: 246, y: 591 },
-  { x: 194, y: 589 },
-  { x: 351, y: 596 },
-  { x: 406, y: 595 },
-  { x: 306, y: 557 },
-  { x: 304, y: 526 },
-  { x: 304, y: 490 },
-  { x: 306, y: 411 },
-  { x: 307, y: 329 },
-  { x: 309, y: 314 },
-  { x: 313, y: 289 },
-  { x: 314, y: 246 },
-  { x: 270, y: 246 },
-  { x: 245, y: 244 },
-  { x: 268, y: 212 },
-  
-  
- 
-];
-
-let pointArray = [];
-
 // ground Array
-let groundPointArray = [
-  { x: 300, y: 595 },
-  { x: 246, y: 591 },
-  { x: 194, y: 589 },
-  { x: 351, y: 596 },
-  { x: 406, y: 595 },
-];
+let groundPointArray = [{ x: 300, y: 595 }, { x: 246, y: 591 }, { x: 194, y: 589 }, { x: 351, y: 596 }, { x: 406, y: 595 }];
 
 // The first branch Arrary
-let firstBranchArray = [
-  { x: 306, y: 557 },
-  { x: 304, y: 526 },
-  { x: 304, y: 490 },
-  { x: 306, y: 411 },
-  { x: 307, y: 329 },];
+let firstBranchArray = [{ x: 306, y: 557 },{ x: 304, y: 526 },{ x: 304, y: 490 },{ x: 306, y: 411 },{ x: 307, y: 329 }];
 
 // The first left Branch
 let leftBranch = [{ x: 127, y: 84 },{ x: 181, y: 258 },{ x: 182, y: 212 }, { x: 131, y: 136 },  { x: 135, y: 174 },  { x: 188, y: 172 }, { x: 180, y: 327 },  { x: 239, y: 328 },  { x: 278, y: 329 }];
@@ -47,17 +11,10 @@ let leftBranch = [{ x: 127, y: 84 },{ x: 181, y: 258 },{ x: 182, y: 212 }, { x: 
 let rightBranch = [{ x: 348, y: 329 },  { x: 384, y: 333 },  { x: 421, y: 333 },  { x: 427, y: 272 },  { x: 429, y: 239 },  { x: 434, y: 183 },  { x: 434, y: 154 },  { x: 461, y: 159 },  { x: 504, y: 172 },  { x: 534, y: 183 },  { x: 539, y: 142 }];
 
 // The second up branch Array
-let secondUpBranch = [
-  { x: 309, y: 314 },
-  { x: 313, y: 289 },
-  { x: 314, y: 246 },];
+let secondUpBranch = [{ x: 309, y: 314 },  { x: 313, y: 289 },  { x: 314, y: 246 }];
 
 // The second left branch
-let secondleftBranch = [
-  { x: 270, y: 246 },
-  { x: 245, y: 244 },
-  { x: 268, y: 212 },
-];
+let secondleftBranch = [{ x: 270, y: 246 },{ x: 245, y: 244 },{ x: 268, y: 212 }];
 
 // The second Right Branch
 let secondRightBranch = [{ x: 360, y: 245 },  { x: 360, y: 216 }];
@@ -84,49 +41,38 @@ function setup() {
   createCanvas(600, 800);
   xPos = width / 2;
   yPos = height / 2;
+  background(25);
 
+
+  for(i=0; i<groundPointArray.length; i++){
+    let p1 = groundPointArray[i];
+    let p2 = groundPointArray[i + 1];
+    let newLine =new Branch(p1, p2, `yellow`, 10)
+    lineArray.push(newLine)
+  }
+}
+
+function draw() {
+  for (let i = 0; i < 1; i++) {
+    drawRandomLine();
+  }
+
+  for (let i = 0; i < lineArray.length; i++) {
+    lineArray[i].display();
+  }
+
+  for (let i = 0;i < groundPointArray.length; i++) {
+    groundPointArray[i].display()
+  }
+}
+
+function drawRandomLine() {
   // Red is delete considering the background
   push();
   r = 0;
   g = random(255);
   b = random(255);
-
-  background(32);
-  for (let pt of rawCoords) {
-    pointArray.push(new Point(pt.x, pt.y, 8));
-  }
   pop();
-
-  lineArray.push(new Branch(300, 595, 300, 250, `yellow`, 5));
-  lineArray.push(new Branch(190, 595, 410, 595, `yellow`, 5));
-  lineArray.push(new Branch(240, 250, 360, 250, `yellow`, 3));
-  lineArray.push(new Branch(270, 250, 270, 220, `yellow`, 3));
-  lineArray.push(new Branch(360, 250, 360, 220, `yellow`, 3));
-  lineArray.push(new Branch(300, 330, 182, 330, `yellow`, 3));
-  lineArray.push(new Branch(182, 330, 182, 178, `yellow`, 3));
-  lineArray.push(new Branch(182, 178, 127, 178, `yellow`, 1));
-  lineArray.push(new Branch(127, 178, 127, 84, `yellow`, 1));
-  lineArray.push(new Branch(300, 330, 420, 330, `yellow`, 3));
-  lineArray.push(new Branch(420, 330, 430, 160, `yellow`, 3));
-  lineArray.push(new Branch(430, 160, 540, 175, `yellow`, 1));
-  lineArray.push(new Branch(540, 175, 540, 150, `yellow`, 1));
-}
-
-function draw() {
- /* for (let i = 0; i < 1; i++) {
-    drawRandomLine();
-  }
-
-  /*for (let i = 0; i < lineArray.length; i++) {
-    lineArray[i].display();
-  }*/
-
-  for (let p of pointArray) {
-    p.display();
-  }
-}
-
-function drawRandomLine() {
   let nextX = xPos + random(-maxLineLength, maxLineLength);
   let nextY = yPos + random(-maxLineLength, maxLineLength);
 
